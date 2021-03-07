bf3a4e9e593b2d0e5491a8626e3dd154df20c5b4
Derrick deVilliers <derrickd44@gmail.com>
2021-03-07 15:04:11 -0500

    Got it functioning properly

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,30 +7,31 @@ const db = require("../db/db.json");
 module.exports = {
     postNotes: function (req, res) {
         const note = { ...req.body, id: uuid() };
-        fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
             if (err) throw err;
             const allNotes = JSON.parse(data);
             allNotes.push(note);
-            fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(allNotes), function (err, data) {
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(allNotes), function (err, data) {
                 if (err) throw err;
-                res.json(data)
+                res.json(allNotes)
             })
         })
     },
     getNotes: function (req, res) {
-        res.json(db);
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
+            if (err) throw err;
+        res.json(JSON.parse(data));
+        })
     },
     deleteNotes: function (req, res) {
         const id = req.params.id;
-        fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function (err, data) {
+        console.log(id);
+        fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, data) {
             if (err) throw err;
             const allNotes = JSON.parse(data);
             const updatedNotes = allNotes.filter((note) => note.id != id );
-            fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(updatedNotes), function (err, data) {
-                if (err) throw err;
-                res.sendFile(path.join(__dirname, "../../public/index.html"))
-
-            })
+            fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(updatedNotes), function () {})
         })
+        res.sendFile(path.join(__dirname, "../public/notes.html"))
     },
 }
